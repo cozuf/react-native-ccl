e1e781152eb8fc73c4561e7004b6a0b05db0d0f0
Yusuf Coşkun <coskunyusuf43@gmail.com>
2021-12-31 18:20:57 +0300

    Theme isimlendirmeleri düzeltildi

--- src/Context/Theme/values.ts
+++ src/Context/Theme/values.ts
@@ -1,8 +1,8 @@
 import type { ThemeType } from "./types";
 import { light } from "../../Theme/Colors";
-import { fonts } from "../..//Theme/Fonts";
-import { componentsStyles } from "../..//Theme/Styles";
-import { tokens } from "../..//Theme";
+import { fonts } from "../../Theme/Fonts";
+import { componentsStyles } from "../../Theme/Styles";
+import { tokens } from "../../Theme";
 
 export const Theme: ThemeType = {
     name: 'Light',
