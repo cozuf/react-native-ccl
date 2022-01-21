e57f9c0fa58c81262314d373a53899a884a7e891
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-01-21 18:25:00 +0300

    icon'a container eklendi

--- src/Components/Icon/index.tsx
+++ src/Components/Icon/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import type { ColorValue } from 'react-native';
+import React, { FC, ReactNode } from 'react';
+import { ColorValue, StyleProp, View, ViewStyle } from 'react-native';
 // @ts-ignore
 import AntDesign from 'react-native-vector-icons/AntDesign';
 // @ts-ignore
@@ -37,21 +37,21 @@ export interface IIconProps {
    *
    */
   family?:
-    | 'AntDesign'
-    | 'Entypo'
-    | 'EvilIcons'
-    | 'Feather'
-    | 'FontAwesome'
-    | 'FontAwesome5'
-    | 'FontAwesome5Pro'
-    | 'Fontisto'
-    | 'Foundation'
-    | 'Ionicons'
-    | 'MaterialCommunityIcons'
-    | 'MaterialIcons'
-    | 'Octicons'
-    | 'SimpleLineIcons'
-    | 'Zocial';
+  | 'AntDesign'
+  | 'Entypo'
+  | 'EvilIcons'
+  | 'Feather'
+  | 'FontAwesome'
+  | 'FontAwesome5'
+  | 'FontAwesome5Pro'
+  | 'Fontisto'
+  | 'Foundation'
+  | 'Ionicons'
+  | 'MaterialCommunityIcons'
+  | 'MaterialIcons'
+  | 'Octicons'
+  | 'SimpleLineIcons'
+  | 'Zocial';
   /**
    * @see https://oblador.github.io/react-native-vector-icons/
    */
@@ -71,6 +71,11 @@ export interface IIconProps {
    * @default true
    */
   active?: boolean;
+
+  /**
+   * 
+   */
+  containerStyle?: StyleProp<ViewStyle>
 }
 
 const Icon: FC<IIconProps> = ({
@@ -79,44 +84,53 @@ const Icon: FC<IIconProps> = ({
   size = 10,
   color,
   active = true,
+  containerStyle
 }) => {
   const [theme] = useTheme();
   const { icon } = theme.colors;
   const COLOR = color ? color : icon[active ? 'active' : 'passive'];
-  switch (family) {
-    case 'AntDesign':
-      return <AntDesign name={name} size={size} color={COLOR} />;
-    case 'Entypo':
-      return <Entypo name={name} size={size} color={COLOR} />;
-    case 'EvilIcons':
-      return <EvilIcons name={name} size={size} color={COLOR} />;
-    case 'Feather':
-      return <Feather name={name} size={size} color={COLOR} />;
-    case 'FontAwesome':
-      return <FontAwesome name={name} size={size} color={COLOR} />;
-    case 'FontAwesome5':
-      return <FontAwesome5 name={name} size={size} color={COLOR} />;
-    case 'FontAwesome5Pro':
-      return <FontAwesome5Pro name={name} size={size} color={COLOR} />;
-    case 'Fontisto':
-      return <Fontisto name={name} size={size} color={COLOR} />;
-    case 'Foundation':
-      return <Foundation name={name} size={size} color={COLOR} />;
-    case 'Ionicons':
-      return <Ionicons name={name} size={size} color={COLOR} />;
-    case 'MaterialCommunityIcons':
-      return <MaterialCommunityIcons name={name} size={size} color={COLOR} />;
-    case 'MaterialIcons':
-      return <MaterialIcons name={name} size={size} color={COLOR} />;
-    case 'Octicons':
-      return <Octicons name={name} size={size} color={COLOR} />;
-    case 'SimpleLineIcons':
-      return <SimpleLineIcons name={name} size={size} color={COLOR} />;
-    case 'Zocial':
-      return <Zocial name={name} size={size} color={COLOR} />;
+
+  const renderIcon = (): ReactNode => {
+    switch (family) {
+      case 'AntDesign':
+        return <AntDesign name={name} size={size} color={COLOR} />;
+      case 'Entypo':
+        return <Entypo name={name} size={size} color={COLOR} />;
+      case 'EvilIcons':
+        return <EvilIcons name={name} size={size} color={COLOR} />;
+      case 'Feather':
+        return <Feather name={name} size={size} color={COLOR} />;
+      case 'FontAwesome':
+        return <FontAwesome name={name} size={size} color={COLOR} />;
+      case 'FontAwesome5':
+        return <FontAwesome5 name={name} size={size} color={COLOR} />;
+      case 'FontAwesome5Pro':
+        return <FontAwesome5Pro name={name} size={size} color={COLOR} />;
+      case 'Fontisto':
+        return <Fontisto name={name} size={size} color={COLOR} />;
+      case 'Foundation':
+        return <Foundation name={name} size={size} color={COLOR} />;
+      case 'Ionicons':
+        return <Ionicons name={name} size={size} color={COLOR} />;
+      case 'MaterialCommunityIcons':
+        return <MaterialCommunityIcons name={name} size={size} color={COLOR} />;
+      case 'MaterialIcons':
+        return <MaterialIcons name={name} size={size} color={COLOR} />;
+      case 'Octicons':
+        return <Octicons name={name} size={size} color={COLOR} />;
+      case 'SimpleLineIcons':
+        return <SimpleLineIcons name={name} size={size} color={COLOR} />;
+      case 'Zocial':
+        return <Zocial name={name} size={size} color={COLOR} />;
+    }
   }
+
+  return (
+    <View style={[containerStyle]}>
+      {renderIcon()}
+    </View>
+  )
+
 };
 
 export default Icon;
-
-// TODO: Duruma göre Container eklenebilir
