1499de5febf55364ceceaac661815eb3eebe6ce2
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-01-21 18:37:01 +0300

    yapılacaklar güncellendi

--- src/index.tsx
+++ src/index.tsx
@@ -73,12 +73,12 @@ export {
 
 // TODO: SelectBox componentini tekrar gözden geçir navigationProps ile ilgili
 // TODO: yayınlamadan önce ReadMe düzenle
-// : GlobalState'yi dışarıdan type verilebilir -> Şimdilik mümkün değil
-// : Language Context -> Burası da GlobalState gibi şimdilik mümkün değil
+// : GlobalState'yi dışarıdan type verilebilir -> global'e yaz
+// : Language Context -> global'e yaz
 // : import - export ları düzenle -> şimdilik mümkün değil ayırmak gerekiyor
 //  --datetimepicker'a moment ile gösterim eklenecek // EKLEME sadece dışarı Date tipini ver
 // TODO: styles'de bulunan tokens'leri ve fontfamily'leri inner yap
 // TODO: --ClassComponent için withTheme yap
-// fucntional component'ta ref kullanmayı öğren - SnackBar'dan öğrenebilirsin!
+// fucntional component'ta ref kullanmayı SnackBar'dan öğrenebilirsin!
 
 // FIXME: Style'lar theme.style'dan color'lar theme.color'dan çekilmeli - gözden geçir unutma!
