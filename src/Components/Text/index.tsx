279487ef7bb0fa589a6b36b8b8755c6b3faff727
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-05-26 16:22:59 +0300

    text component size prop explanantion added

--- src/Components/Text/index.tsx
+++ src/Components/Text/index.tsx
@@ -18,6 +18,13 @@ export interface ITextProps {
 
   /**
    * Text Sizes
+   * xxs = 8
+   * xs = 10
+   * s = 12
+   * m = 14
+   * l = 16
+   * xl = 18
+   * xxl = 20
    */
   size?: 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' | number
 
