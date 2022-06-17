0df69fd68289c78efa91565d74edefcd72de8fe0
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-06-18 00:11:40 +0300

    RNCCL provider fixed

--- src/Context/RNCCL/provider.tsx
+++ src/Context/RNCCL/provider.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from "react"
-import { light, fonts, tokens } from "../../Theme"
+import { light, fonts as defaultFonts, tokens as defaultTokens } from "../../Theme"
 import { BottomSheetProvider } from "../BottomSheet"
 import { GlobalStateProvider } from "../GlobalState"
 import { ModalProvider } from "../Modal"
 import { ThemeProvider } from "../Theme"
 
 const RNCCLProvider: FC<RNCCLScheme> = ({ theme, globalState, children }) => {
+    const { name, colors, fonts, tokens } = theme
     return (
         <GlobalStateProvider initialGobalState={globalState}>
-            <ThemeProvider name={theme.name || "light"} colors={theme.colors || light} fonts={theme.fonts || fonts} tokens={theme.tokens || tokens}>
+            <ThemeProvider name={name || "light"} colors={colors || light} fonts={fonts || defaultFonts} tokens={tokens || defaultTokens}>
                 <ModalProvider>
                     <BottomSheetProvider>
                         {children}
