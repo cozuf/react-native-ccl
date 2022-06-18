e4e04c7a18921bec7c8bc674332f8524619e07b7
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-06-19 02:10:30 +0300

    TODO's fixed

--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,6 @@ export {
 // TODO: Badge platform farklılıklarını gözden geçir ve onayla bitir
 // TODO: SearchBar onClear ayrılabilir mi bak gerekiyorsa ayır ve bitir
 // TODO: inputs'lar için infoButton yap SnackBar ile
-// TODO: SnackBar çeşitlendir - (arkadan öne - önden arkaya) - (aşağıdan yukarıya - yukarıdan aşağıya) - (soldan sağa - sağdan sola)
 
 
 // TODO: yayınlamadan önce ReadMe düzenle
