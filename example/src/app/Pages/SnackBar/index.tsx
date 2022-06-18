726cb996e2004a65e92d5241daf182b792a9cf35
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-06-19 02:06:57 +0300

    Snackbar component redesigned

--- example/src/app/Pages/SnackBar/index.tsx
+++ example/src/app/Pages/SnackBar/index.tsx
@@ -1,24 +1,87 @@
 import React, { RefObject, useRef, useState } from 'react';
-import { Button, PageContainer, Seperator, SnackBar, SnackBarRef } from 'react-native-ccl';
+import { Button, ISnackBarProps, PageContainer, Seperator, SnackBar, SnackBarRef, TapSelector } from 'react-native-ccl';
+
+const FORMS = [
+    {
+        title: "backToFront"
+    },
+    {
+        title: "leftToRight"
+    },
+    {
+        title: "rightToLeft"
+    },
+    {
+        title: "bottomToTop"
+    },
+    {
+        title: "hideToShow"
+    },
+]
+
+const DURATIONS = [
+    {
+        title: "infinite"
+    },
+    {
+        title: "short"
+    },
+    {
+        title: "medium"
+    },
+    {
+        title: "long"
+    }
+]
 
 const SnackBarPage = () => {
     const snackBarRef = useRef<SnackBarRef>() as RefObject<SnackBarRef>
-    const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState<boolean | null>(false)
+    const [formIndex, setFormIndex] = useState<number>(0);
+    const [durationIndex, setDurationIndex] = useState<number>(0);
 
     return (
         <PageContainer type="default">
-            <Button title="Show" disabled={visible} onPress={() => {
-                snackBarRef.current?.show()
-                setVisible(true)
-            }} />
+            <TapSelector
+                containerStyle={{ marginBottom: 8 }}
+                data={FORMS}
+                onTap={(_, index) => {
+                    setFormIndex(index);
+                }}
+            />
+            <TapSelector
+                containerStyle={{ marginBottom: 8 }}
+                data={DURATIONS}
+                onTap={(_, index) => {
+                    setDurationIndex(index);
+                }}
+            />
+            <Button
+                title="Show"
+                disabled={visible !== false}
+                onPress={() => {
+                    snackBarRef.current?.show()
+                    setVisible(null)
+                }}
+            />
 
             <Seperator type='vertical' />
 
-            <Button title="Close" disabled={!visible} onPress={() => {
-                snackBarRef.current?.close()
-                setVisible(false)
-            }} />
-            <SnackBar ref={snackBarRef} duration="long" />
+            <Button
+                title="Close"
+                disabled={visible !== true}
+                onPress={() => {
+                    snackBarRef.current?.close()
+                    setVisible(null)
+                }}
+            />
+            <SnackBar
+                ref={snackBarRef}
+                duration={DURATIONS[durationIndex].title as ISnackBarProps["duration"]}
+                displayForm={FORMS[formIndex].title as ISnackBarProps["displayForm"]}
+                onCompleteHide={() => { setVisible(false) }}
+                onCompleteShow={() => { setVisible(true) }}
+            />
         </PageContainer>
     );
 };
--- src/Components/SnackBar/index.tsx
+++ src/Components/SnackBar/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useImperativeHandle, useState, forwardRef, Ref, PropsWithChildren, useRef } from "react";
-import { Animated, Dimensions, Easing, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { Animated, Easing, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { SCREEN_WIDTH, WINDOW_WIDTH } from "../../Utils";
 import { useTheme } from "../../Context";
 
 const SHORT_DURATION = 1000
@@ -8,8 +9,6 @@ const LONG_DURATION = 5000
 const INFINITE_DURATION = -1
 
 const SHOWING_TIME = 700
-
-const WIDTH = Dimensions.get("window").width
 
 // https://github.com/cooperka/react-native-snackbar bunu da kullanabilirsin
 export interface SnackBarRef {
@@ -26,7 +25,22 @@ export interface ISnackBarProps {
     /**
      * 
      */
+    displayForm?: "hideToShow" | "backToFront" | "leftToRight" | "rightToLeft" | "bottomToTop"
+
+    /**
+     * 
+     */
     containerStyle?: StyleProp<ViewStyle>
+
+    /**
+     * 
+     */
+    onCompleteShow?: () => void
+
+    /**
+     * 
+     */
+    onCompleteHide?: () => void
 }
 
 const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<SnackBarRef>) => {
@@ -35,11 +49,34 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
     const { snackBar } = colors;
     const { component } = tokens;
 
-    const { duration, containerStyle, children } = props
+    const { duration, displayForm = "backToFront", containerStyle, onCompleteHide = () => { }, onCompleteShow = () => { }, children } = props
+
+    const left = useRef(new Animated.Value(-SCREEN_WIDTH)).current
+    const right = useRef(new Animated.Value(-SCREEN_WIDTH)).current
+    const bottom = useRef(new Animated.Value(-66)).current
     const opacity = useRef(new Animated.Value(0)).current
     const zIndex = useRef(new Animated.Value(-1)).current
     const scale = useRef(new Animated.Value(0)).current
-    const [visible, setVisible] = useState<boolean>(false)
+
+    const [state, setState] = useState<"SHOWING" | "SHOWED" | "CLOSING" | "CLOSED">("CLOSED")
+
+    const [isLeftStartComplete, setIsLeftStartComplete] = useState<boolean>(false)
+    const [isLeftFinishComplete, setIsLeftFinishComplete] = useState<boolean>(true)
+
+    const [isRightStartComplete, setIsRightStartComplete] = useState<boolean>(false)
+    const [isRightFinishComplete, setIsRightFinishComplete] = useState<boolean>(true)
+
+    const [isBottomStartComplete, setIsBottomStartComplete] = useState<boolean>(false)
+    const [isBottomFinishComplete, setIsBottomFinishComplete] = useState<boolean>(true)
+
+    const [isOpacityStartComplete, setIsOpacityStartComplete] = useState<boolean>(false)
+    const [isOpacityFinishComplete, setIsOpacityFinishComplete] = useState<boolean>(true)
+
+    const [isZIndexStartComplete, setIsZIndexStartComplete] = useState<boolean>(false)
+    const [isZIndexFinishComplete, setIsZIndexFinishComplete] = useState<boolean>(true)
+
+    const [isScaleStartComplete, setIsScaleStartComplete] = useState<boolean>(false)
+    const [isScaleFinishComplete, setIsScaleFinishComplete] = useState<boolean>(true)
 
     useImperativeHandle(ref, () => ({
         show: show,
@@ -47,7 +84,8 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
     }));
 
     useEffect(() => {
-        if (visible) {
+        if (state === "SHOWED") {
+            onCompleteShow()
             let DURATION: number;
             switch (duration) {
                 case "short":
@@ -64,9 +102,62 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
                     DURATION = INFINITE_DURATION
                     break;
             }
-            setTimeout(() => { close() }, DURATION);
+            if (DURATION !== INFINITE_DURATION) {
+                setTimeout(close, DURATION)
+            }
         }
-    }, [visible])
+        if (state === "CLOSED") {
+            onCompleteHide()
+        }
+    }, [state])
+
+    useEffect(() => {
+        if (state === "CLOSED" || state === "SHOWING") {
+            if (displayForm === "backToFront") {
+                setState((oldValue) => isOpacityStartComplete && isZIndexStartComplete && isScaleStartComplete ? "SHOWED" : oldValue)
+            }
+
+            if (displayForm === "leftToRight") {
+                setState((oldValue) => isOpacityStartComplete && isLeftStartComplete ? "SHOWED" : oldValue)
+            }
+
+            if (displayForm === "rightToLeft") {
+                setState((oldValue) => isOpacityStartComplete && isRightStartComplete ? "SHOWED" : oldValue)
+            }
+
+            if (displayForm === "bottomToTop") {
+                setState((oldValue) => isOpacityStartComplete && isBottomStartComplete ? "SHOWED" : oldValue)
+            }
+
+            if (displayForm === "hideToShow") {
+                setState((oldValue) => isOpacityStartComplete ? "SHOWED" : oldValue)
+            }
+        }
+    }, [isLeftStartComplete, isRightStartComplete, isBottomStartComplete, isOpacityStartComplete, isZIndexStartComplete, isScaleStartComplete])
+
+    useEffect(() => {
+        if (state === "SHOWED" || state === "CLOSING") {
+            if (displayForm === "backToFront") {
+                setState((oldValue) => isOpacityFinishComplete && isZIndexFinishComplete && isScaleFinishComplete ? "CLOSED" : oldValue)
+            }
+
+            if (displayForm === "leftToRight") {
+                setState((oldValue) => isOpacityFinishComplete && isLeftFinishComplete ? "CLOSED" : oldValue)
+            }
+
+            if (displayForm === "rightToLeft") {
+                setState((oldValue) => isOpacityFinishComplete && isRightFinishComplete ? "CLOSED" : oldValue)
+            }
+
+            if (displayForm === "bottomToTop") {
+                setState((oldValue) => isOpacityFinishComplete && isBottomFinishComplete ? "CLOSED" : oldValue)
+            }
+
+            if (displayForm === "hideToShow") {
+                setState((oldValue) => isOpacityFinishComplete ? "CLOSED" : oldValue)
+            }
+        }
+    }, [isLeftFinishComplete, isRightFinishComplete, isBottomFinishComplete, isOpacityFinishComplete, isZIndexFinishComplete, isScaleFinishComplete])
 
     //#region FADE
     const fadeIn = () => {
@@ -77,7 +168,10 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
                 toValue: 1,
                 useNativeDriver: false,
             }
-        ).start();
+        ).start(({ finished }) => {
+            setIsOpacityStartComplete(finished)
+            setIsOpacityFinishComplete(!finished)
+        });
     }
     const fadeOut = () => {
         Animated.timing(
@@ -88,30 +182,39 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
                 easing: Easing.back(0),
                 useNativeDriver: false,
             }
-        ).start();
+        ).start(({ finished }) => {
+            setIsOpacityFinishComplete(finished)
+            setIsOpacityStartComplete(!finished)
+        });
     }
     //#endregion
 
-    //#region BRING
+    //#region ELEVATION
     const front = () => {
         Animated.timing(
             zIndex,
             {
                 duration: SHOWING_TIME,
-                toValue: 1,
+                toValue: 99,
                 useNativeDriver: false,
             }
-        ).start();
+        ).start(({ finished }) => {
+            setIsZIndexStartComplete(finished)
+            setIsZIndexFinishComplete(!finished)
+        });
     }
     const back = () => {
         Animated.timing(
             zIndex,
             {
                 duration: SHOWING_TIME,
-                toValue: -1,
+                toValue: -99,
                 useNativeDriver: false,
             }
-        ).start();
+        ).start(({ finished }) => {
+            setIsZIndexFinishComplete(finished)
+            setIsZIndexStartComplete(!finished)
+        });
     }
     //#endregion
 
@@ -124,7 +227,10 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
                 toValue: 1,
                 useNativeDriver: false,
             }
-        ).start();
+        ).start(({ finished }) => {
+            setIsScaleStartComplete(finished)
+            setIsScaleFinishComplete(!finished)
+        });
     }
     const scaleOut = () => {
         Animated.timing(
@@ -134,22 +240,152 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
                 toValue: 0,
                 useNativeDriver: false,
             }
-        ).start();
+        ).start(({ finished }) => {
+            setIsScaleFinishComplete(finished)
+            setIsScaleStartComplete(!finished)
+        });
+    }
+    //#endregion
+
+    //#region LEFT_TO_RIGHT
+    const fromLeft = () => {
+        Animated.timing(
+            left,
+            {
+                duration: SHOWING_TIME,
+                toValue: 0,
+                useNativeDriver: false,
+            }
+        ).start(({ finished }) => {
+            setIsLeftStartComplete(finished)
+            setIsLeftFinishComplete(!finished)
+        });
+    }
+    const toLeft = () => {
+        Animated.timing(
+            left,
+            {
+                duration: SHOWING_TIME,
+                toValue: -SCREEN_WIDTH,
+                useNativeDriver: false,
+            }
+        ).start(({ finished }) => {
+            setIsLeftFinishComplete(finished)
+            setIsLeftStartComplete(!finished)
+        });
+    }
+    //#endregion
+
+    //#region RIGHT_TO_LEFT
+    const fromRgiht = () => {
+        Animated.timing(
+            right,
+            {
+                duration: SHOWING_TIME,
+                toValue: 0,
+                useNativeDriver: false,
+            }
+        ).start(({ finished }) => {
+            setIsRightStartComplete(finished)
+            setIsRightFinishComplete(!finished)
+        });
+    }
+    const toRgiht = () => {
+        Animated.timing(
+            right,
+            {
+                duration: SHOWING_TIME,
+                toValue: -SCREEN_WIDTH,
+                useNativeDriver: false,
+            }
+        ).start(({ finished }) => {
+            setIsRightFinishComplete(finished)
+            setIsRightStartComplete(!finished)
+        });
+    }
+    //#endregion
+
+    //#region BOTTOM_TO_TOP
+    const comeUp = () => {
+        Animated.timing(
+            bottom,
+            {
+                duration: SHOWING_TIME,
+                toValue: 0,
+                useNativeDriver: false,
+            }
+        ).start(({ finished }) => {
+            setIsBottomStartComplete(finished)
+        });
+    }
+    const goDown = () => {
+        Animated.timing(
+            bottom,
+            {
+                duration: SHOWING_TIME,
+                toValue: -66,
+                useNativeDriver: false,
+            }
+        ).start(({ finished }) => {
+            setIsBottomFinishComplete(finished)
+        });
     }
     //#endregion
 
     const show = () => {
-        fadeIn()
-        front()
-        scaleIn()
-        setVisible(true)
+        setState("SHOWING")
+        if (displayForm === "backToFront") {
+            fadeIn()
+            front()
+            scaleIn()
+        }
+
+        if (displayForm === "leftToRight") {
+            fadeIn()
+            fromLeft()
+        }
+
+        if (displayForm === "rightToLeft") {
+            fadeIn()
+            fromRgiht()
+        }
+
+        if (displayForm === "bottomToTop") {
+            fadeIn()
+            comeUp()
+        }
+
+        if (displayForm === "hideToShow") {
+            fadeIn()
+        }
     }
 
     const close = () => {
-        fadeOut()
-        back()
-        scaleOut()
-        setVisible(false)
+        setState("CLOSING")
+        if (displayForm === "backToFront") {
+            fadeOut()
+            back()
+            scaleOut()
+        }
+
+        if (displayForm === "leftToRight") {
+            fadeOut()
+            toLeft()
+        }
+
+        if (displayForm === "rightToLeft") {
+            fadeOut()
+            toRgiht()
+        }
+
+        if (displayForm === "bottomToTop") {
+            fadeOut()
+            goDown()
+        }
+
+        if (displayForm === "hideToShow") {
+            fadeOut()
+        }
     }
 
     return (
@@ -160,11 +396,39 @@ const SnackBar = forwardRef((props: PropsWithChildren<ISnackBarProps>, ref: Ref<
                     {
                         paddingVertical: component.vertical,
                         paddingHorizontal: component.horizontal,
-                        width: WIDTH,
-                        zIndex: zIndex,
+                        width: WINDOW_WIDTH,
                         opacity: opacity,
-                        transform: [{ scale }],
-                    }
+                    },
+                    displayForm === "backToFront"
+                        ?
+                        {
+                            zIndex: zIndex,
+                            transform: [{ scale }],
+                        }
+                        :
+                        displayForm === "leftToRight"
+                            ?
+                            {
+                                left: left
+                            }
+                            :
+                            displayForm === "rightToLeft"
+                                ?
+                                {
+                                    right: right
+                                }
+                                :
+                                displayForm === "bottomToTop"
+                                    ?
+                                    {
+                                        bottom: bottom
+                                    }
+                                    :
+                                    {
+                                        bottom: 0,
+                                        left: 0,
+                                        right: 0
+                                    }
                 ]
             }
         >
