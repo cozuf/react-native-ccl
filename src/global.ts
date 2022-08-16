637d90eaeb2ddd274d3e87bda7dd4d40a4e13cad
Yusuf Coşkun <coskunyusuf43@gmail.com>
2022-08-16 04:38:25 +0300

    types fixed

--- src/global.ts
+++ src/global.ts
@@ -3,187 +3,189 @@ import type { ColorValue } from 'react-native'
 import type { IModalProps, IBottomSheetProps, ISnackBarProps } from './Components'
 
 declare global {
+    namespace RNCCL {
 
-    //#region ColorScheme
+        //#region ColorScheme
 
-    export interface ColorScheme {
-        brand: ColorValue
-        primary: ColorValue
-        secondary: ColorValue
-        tertiary: ColorValue
-        success: ColorValue
-        warning: ColorValue
-        error: ColorValue
-        white: ColorValue
-        black: ColorValue
-        transparent: ColorValue
+        interface ColorScheme {
+            brand: ColorValue
+            primary: ColorValue
+            secondary: ColorValue
+            tertiary: ColorValue
+            success: ColorValue
+            warning: ColorValue
+            error: ColorValue
+            white: ColorValue
+            black: ColorValue
+            transparent: ColorValue
 
-        pageBackground: ColorValue
+            pageBackground: ColorValue
 
-        componentBackground: ColorValue
-        componentBorder: ColorValue
-        componentIcon: ColorValue
-        componentTitle: ColorValue
-        componentValue: ColorValue
+            componentBackground: ColorValue
+            componentBorder: ColorValue
+            componentIcon: ColorValue
+            componentTitle: ColorValue
+            componentValue: ColorValue
 
-        buttonText: ColorValue
+            buttonText: ColorValue
 
-        /**
-         * selectBox listItemSeperator
-         */
-        listItemSeperator: ColorValue
+            /**
+             * selectBox listItemSeperator
+             */
+            listItemSeperator: ColorValue
 
-        modalOutside: ColorValue
-        modalBackground: ColorValue
+            modalOutside: ColorValue
+            modalBackground: ColorValue
 
-        shadow: ColorValue
+            shadow: ColorValue
 
-        text: ColorValue
-        placeholder: ColorValue
-    }
-
-    //#endregion
-
-    //#region FontScheme
-
-    export interface FontScheme {
-        light: string
-        regular: string
-        medium: string
-        semibold: string
-        bold: string
-    }
-
-    //#endregion
-
-    //#region TokenScheme
-
-    export interface TokenScheme {
-        innerSpace: {
-            pageVertical: number
-            pageHorizontal: number
-            componentVertical: number
-            componentHorizontal: number
-
-            thin: number
-            semiThin: number
-            regular: number
-            semiLarge: number
-            large: number
+            text: ColorValue
+            placeholder: ColorValue
         }
-        outerSpace: {
-            componentVertical: number
-            componentHorizontal: number
 
-            thin: number
-            semiThin: number
-            regular: number
-            semiLarge: number
-            large: number
+        //#endregion
+
+        //#region FontScheme
+
+        interface FontScheme {
+            light: string
+            regular: string
+            medium: string
+            semibold: string
+            bold: string
         }
-        borders: {
-            badge: number
-            button: number
-            component: number
-            textInputFocused: number
 
-            thin: number
-            semiThin: number
-            regular: number
-            semiLarge: number
-            large: number
+        //#endregion
+
+        //#region TokenScheme
+
+        interface TokenScheme {
+            innerSpace: {
+                pageVertical: number
+                pageHorizontal: number
+                componentVertical: number
+                componentHorizontal: number
+
+                thin: number
+                semiThin: number
+                regular: number
+                semiLarge: number
+                large: number
+            }
+            outerSpace: {
+                componentVertical: number
+                componentHorizontal: number
+
+                thin: number
+                semiThin: number
+                regular: number
+                semiLarge: number
+                large: number
+            }
+            borders: {
+                badge: number
+                button: number
+                component: number
+                textInputFocused: number
+
+                thin: number
+                semiThin: number
+                regular: number
+                semiLarge: number
+                large: number
+            }
+            radiuses: {
+                component: number
+
+                thin: number
+                semiThin: number
+                regular: number
+                semiLarge: number
+                large: number
+            }
         }
-        radiuses: {
-            component: number
+        //#endregion
 
-            thin: number
-            semiThin: number
-            regular: number
-            semiLarge: number
-            large: number
+        //#region ThemeScheme
+        interface ThemeScheme {
+            name: 'Light' | 'Dark'
+            colors: ColorScheme
+            fonts: FontScheme
+            tokens: TokenScheme
         }
+
+        interface ITheme {
+            theme: ThemeScheme
+            setTheme: Dispatch<Partial<ThemeScheme>>
+        }
+        //#endregion
+
+        //#region GlobalStateScheme
+
+        interface GlobalStateScheme { }
+
+        interface IGlobalState {
+            globalState: Partial<GlobalStateScheme>
+            setGlobalState: Dispatch<Partial<GlobalStateScheme>>
+        }
+        //#endregion
+
+        //#region BottomSheetScheme
+        interface BottomSheetScheme {
+            show: () => void,
+            close: () => void,
+        }
+
+        interface SetBottomSheetScheme {
+            props: IBottomSheetProps,
+            renderContent?: () => ReactNode | null
+        }
+
+        interface IBottomSheetScheme {
+            bottomSheet: BottomSheetScheme
+            setBottomSheet: Dispatch<Partial<SetBottomSheetScheme>>
+        }
+        //#endregion
+
+        //#region ModalScheme
+        interface ModalScheme {
+            show: () => void,
+            close: () => void,
+        }
+
+        interface SetModalScheme {
+            props: Omit<IModalProps, "visible">,
+            renderChildren?: () => ReactNode | null
+        }
+
+        interface IModalScheme {
+            modal: ModalScheme
+            setModal: Dispatch<Partial<SetModalScheme>>
+        }
+        //#endregion
+
+        // #region SnackBarScheme
+        interface SnackBarScheme {
+            show: () => void,
+            close: () => void,
+        }
+
+        interface SetSnackBarScheme {
+            props: ISnackBarProps,
+            renderChildren?: () => ReactNode | null
+        }
+
+        interface ISnackBarScheme {
+            snackBar: SnackBarScheme
+            setSnacBar: Dispatch<Partial<SetSnackBarScheme>>
+        }
+        // #endregion
+
+        //#region RNCCLScheme
+        interface RNCCLScheme {
+            theme: ThemeScheme,
+            globalState: GlobalStateScheme
+        }
+        //#endregion
     }
-    //#endregion
-
-    //#region ThemeScheme
-    export interface ThemeScheme {
-        name: 'Light' | 'Dark'
-        colors: ColorScheme
-        fonts: FontScheme
-        tokens: TokenScheme
-    }
-
-    export interface ITheme {
-        theme: ThemeScheme
-        setTheme: Dispatch<Partial<ThemeScheme>>
-    }
-    //#endregion
-
-    //#region GlobalStateScheme
-
-    export interface GlobalStateScheme { }
-
-    export interface IGlobalState {
-        globalState: Partial<GlobalStateScheme>
-        setGlobalState: Dispatch<Partial<GlobalStateScheme>>
-    }
-    //#endregion
-
-    //#region BottomSheetScheme
-    export interface BottomSheetScheme {
-        show: () => void,
-        close: () => void,
-    }
-
-    export interface SetBottomSheetScheme {
-        props: IBottomSheetProps,
-        renderContent?: () => ReactNode | null
-    }
-
-    export interface IBottomSheetScheme {
-        bottomSheet: BottomSheetScheme
-        setBottomSheet: Dispatch<Partial<SetBottomSheetScheme>>
-    }
-    //#endregion
-
-    //#region ModalScheme
-    export interface ModalScheme {
-        show: () => void,
-        close: () => void,
-    }
-
-    export interface SetModalScheme {
-        props: Omit<IModalProps, "visible">,
-        renderChildren?: () => ReactNode | null
-    }
-
-    export interface IModalScheme {
-        modal: ModalScheme
-        setModal: Dispatch<Partial<SetModalScheme>>
-    }
-    //#endregion
-
-    // #region SnackBarScheme
-    export interface SnackBarScheme {
-        show: () => void,
-        close: () => void,
-    }
-
-    export interface SetSnackBarScheme {
-        props: ISnackBarProps,
-        renderChildren?: () => ReactNode | null
-    }
-
-    export interface ISnackBarScheme {
-        snackBar: SnackBarScheme
-        setSnacBar: Dispatch<Partial<SetSnackBarScheme>>
-    }
-    // #endregion
-
-    //#region RNCCLScheme
-    export interface RNCCLScheme {
-        theme: ThemeScheme,
-        globalState: GlobalStateScheme
-    }
-    //#endregion
 }
